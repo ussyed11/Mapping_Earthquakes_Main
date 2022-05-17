4a91306f94208c9bade9050cbe06d0a63042b048
[Uzma Syed] <[uzsasyed@gmail.com]>
2022-05-17 14:39:14 -0400

    Adding Totonto data file

---
+++ Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,36 @@
+//We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Light: light,
+  Dark: dark
+};
+
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom:2,
+  layers: [light]
+});
+L.control.layers(baseMaps).addTo(map);
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/ussyed11/Mapping_Earthquakes/main/majorAirports.json";
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Airport code: " + feature.properties.faa + "</h3> <hr><h3> Airport name: " + feature.properties.name + "</h3>");
+     }  
+  }).addTo(map);
+});
